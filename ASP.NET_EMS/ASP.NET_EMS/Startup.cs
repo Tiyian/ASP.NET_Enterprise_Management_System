f6f4fd8423ab6122521dd3066b244c37f6b71df6
ZoraGK <jyh199759@126.com>
2019-03-01 16:49:54 +0800

    Create

---
+++ ASP.NET_EMS/ASP.NET_EMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_EMS.Startup))]
+namespace ASP.NET_EMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
